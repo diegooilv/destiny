51f35c70d5e251dc1a56c079d003419ef2ed89aa
DiegoOliv07 <diego.oliv2007@gmail.com>
2024-12-29 14:23:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
-const config = require('./config.json');
 
 const token = process.env.TOKEN;
+const SimpDB = require('simpl.db');
+const db = new SimpDB({
+  filepath: './database.json',  
+  autosave: true
+});
+
+const bot = db.get('bot');
+const prefix = bot.prefix;
+console.log(prefix);
 
 const client = new Client({
   intents: [
@@ -18,7 +26,6 @@ client.commands = new Map();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// Carrega os comandos da pasta 'commands'
 for (const file of commandFiles) {
   const command = require(path.join(commandsPath, file));
   client.commands.set(command.data.name, command);
@@ -28,28 +35,26 @@ client.once('ready', () => {
   console.log('Bot está online!');
 });
 
-// Lida com comandos de prefixo
 client.on('messageCreate', message => {
-  if (message.author.bot) return;  // Ignora mensagens de outros bots
-  
-  // Verifica se a mensagem começa com o prefixo configurado
-  if (!message.content.startsWith(config.prefix)) return;
+  if (message.author.bot) return;  
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/);  
+  if (message.mentions.has(client.user)) {
+    message.reply(`**(⚡) - ${message.author.username}**, você me marcou? Use \`${prefix}ajuda\` para mais informações.`)};
+  if (!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);  
   const commandName = args.shift().toLowerCase();  
 
-  // Verifica se o comando existe
   if (!client.commands.has(commandName)) return;
 
   const command = client.commands.get(commandName);
 
   try {
-    command.execute(client, message, args);  // Executa o comando
+    command.execute(client, message, args);  
   } catch (error) {
     console.error(error);
     message.reply('Houve um erro ao tentar executar esse comando!');
   }
 });
 
-// Login do bot com o token
 client.login(token);
